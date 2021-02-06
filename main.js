b964c1177733c1d0e80c36b727efa24911e28ca5
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-02-06 14:06:10 +0330

    copy pase all code to find out how its work

---
+++ main.js
@@ -0,0 +1,17 @@
+
+document.querySelector('.nav__btn').addEventListener('click', () => {
+    document.querySelector('.hero__bg').classList.toggle('hero-active');
+    
+    if(document.querySelector('.hero__bg').classList.contains('hero-active')) {
+        document.querySelector('.hero__content').classList.add('hero-content');
+    }else{
+        document.querySelector('.hero__content').classList.remove('hero-content');
+    };
+})
+
+
+    
+
+// if (screen.width <= 900){
+//     console.log('its not imposiable')
+// }
